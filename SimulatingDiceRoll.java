c8380b42c8549c35e277beaf7b8ef42fc349cefb
Shafayet Jamil <144683352+Shafayet-jamil@users.noreply.github.com>
2025-01-12 20:40:25 +0600

    Add files via upload
    Adding Simulation Dice Role file

--- SimulatingDiceRoll.java
+++ SimulatingDiceRoll.java
@@ -2,6 +2,11 @@ import java.util.Random;
 
 public class SimulatingDiceRoll {
     public static void main(String[] args) {
+       Random rand = new Random();
+//       int one = rand.nextInt(10);
+       int num = rand.nextInt(6, 100);
 
+       System.out.println("The number rolled is " + num);
+//        System.out.println("The number rolled is " + one);
     }
 }
